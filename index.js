62571ee46dbc27173e915f66a269854c0d50f7f7
Nicolas <nicolasmilloco@hotmail.com>
2021-10-20 11:50:18 +0200

    fetch json

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ async function loadData() {
     const response = await fetch('./fishEye.json');
     const data = await re.json();
     console.log(data); 
-    // logs [{ name: 'Joker'}, { name: 'Batman' }]
   }
   
   loadData();
