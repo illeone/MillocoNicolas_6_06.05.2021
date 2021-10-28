612fd9f6852c1130647a348fc7411dc2a6d9a341
Nicolas <nicolasmilloco@hotmail.com>
2021-10-28 17:17:50 +0200

    class Photographer

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 async function loadData() {
     const response = await fetch('./fishEye.json');
-    const data = await re.json();
-    console.log(data); 
+    const data = await response.json();
+    return data; 
   }
-  
-  loadData();
+
+  const init = async() => {
+    const data = await loadData();
+    const photographers = data.photographers;
+    console.log(photographers);
+    let unPhotographe = new Photographer(photographers[1])
+    console.log(unPhotographe.createHTML());
+  }
+  init();
+
+
+
+
+
