1979b083d91aba9a4241a8e52889dd4400e4d317
Nicolas <nicolasmilloco@hotmail.com>
2021-11-03 13:00:32 +0100

    fonction createHTML (inner html)

--- Photographer.js
+++ Photographer.js
@@ -9,4 +9,20 @@ class Photographer{
         this.price = data.price
         this.portrait = data.portrait
     }
+
+    createHTML() {
+
+        return `
+        <div class= "photographe">
+                <a class="photographer__profil">
+                    <p class="id">${this.id}</p>
+                    <img class="vignette" src="photos/ID/${this.portrait}" alt="">
+                    <h2 class="photographer__profil__name">${this.name}</h2>
+                </a>
+                <p class="location">${this.city}, ${this.country}</p>
+                <p class="tagline">${this.tagline}</p>
+                <p class="price">${this.price}€/jour</p>
+        </div>`
+
+    }
 }
