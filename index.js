e8186b9bf6ecaf9a0ecf4e27647a4fb69d909a31
Nicolas <nicolasmilloco@hotmail.com>
2021-11-03 12:59:21 +0100

    objet Photographer (constructor)

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-async function loadData() {
+  const loadData = async () => {
     const response = await fetch('./fishEye.json');
     const data = await response.json();
     return data; 
   }
 
-  
+
   const init = async() => {
     const data = await loadData();
     const photographers = data.photographers;
     console.log(photographers);
-    let unPhotographe = new Photographer(photographers[1])
-    console.log(unPhotographe.createHTML());
+    dysplayPhotographer(photographers)
+    
+
+    // let unPhotographe = new Photographer(photographers[5]);
+    //   // for( var i = 0; i < 6; i++);
+    // console.table(photographers)
+    // console.log(unPhotographe.createHTML());
+    
   }
   init();
 
+  // let some = 10
+  // let a = 12
 
-
-
+  // some = a + some 
 
